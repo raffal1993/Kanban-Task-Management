b7c9326001cc3a1cd4d0071d800606e6b4a31df8
raffal1993 <raffal1993@gmail.com>
2023-09-03 21:38:18 +0200

    add: mocked data

---
+++ src/app/data.model.ts
@@ -0,0 +1,25 @@
+interface RootObject {
+  boards: Board[];
+}
+
+interface Board {
+  name: string;
+  columns: Column[];
+}
+
+interface Column {
+  name: string;
+  tasks: Task[];
+}
+
+interface Task {
+  title: string;
+  description: string;
+  status: string;
+  subtasks: Subtask[];
+}
+
+interface Subtask {
+  title: string;
+  isCompleted: boolean;
+}
